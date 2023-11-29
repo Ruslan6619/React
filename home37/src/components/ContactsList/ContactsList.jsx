906a000d2fd3ab6238d39688491ad299e6509af7
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-11-30 00:42:22 +0200

    hw39

--- home37/src/components/ContactsList/ContactsList.jsx
+++ home37/src/components/ContactsList/ContactsList.jsx
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ContactsList = ({ contacts, onDeleteContact }) => (
-    <div>
-        <h2>Список контактов</h2>
-        <table>
-            <thead>
-            <tr>
-                <th>Имя</th>
-                <th>Фамилия</th>
-                <th>Телефон</th>
-                <th>Удалить</th>
-            </tr>
-            </thead>
-            <tbody>
-            {contacts.map((contact, index) => (
-                <tr key={index}>
-                    <td>{contact.name}</td>
-                    <td>{contact.surname}</td>
-                    <td>{contact.phone}</td>
-                    <td>
-                        <button onClick={() => onDeleteContact(contact.id)}>Удалить</button>
-                    </td>
+const ContactsList = ({ contacts, onDeleteContact }) => {
+    const [selectedContact, setSelectedContact] = useState(null);
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const openModal = (contact) => {
+        setSelectedContact(contact);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedContact(null);
+        setModalOpen(false);
+    };
+
+    const handleDelete = () => {
+        onDeleteContact(selectedContact.id);
+        closeModal();
+    };
+
+    return (
+        <div>
+            <h2>Список контактов</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Имя</th>
+                    <th>Фамилия</th>
+                    <th>Телефон</th>
+                    <th>Удалить</th>
                 </tr>
-            ))}
-            </tbody>
-        </table>
-    </div>
-);
+                </thead>
+                <tbody>
+                {contacts.map((contact, index) => (
+                    <tr key={index}>
+                        <td>{contact.name}</td>
+                        <td>{contact.surname}</td>
+                        <td>{contact.phone}</td>
+                        <td>
+                            <button onClick={() => openModal(contact)}>Удалить</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
 
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <h2>Вы уверены, что хотите удалить этот контакт?</h2>
+                        <p>{selectedContact && `${selectedContact.name} ${selectedContact.surname}`}</p>
+                        <button onClick={handleDelete}>Да, удалить</button>
+                        <button onClick={closeModal}>Отмена</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default ContactsList;
